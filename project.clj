813eea687908d88266f11ed9b9ee083545fe9d1a
Thomas Kristensen <thomas.kristensen@uswitch.com>
2013-10-07 17:36:27 +0100

    Initial commit with very basic functionality

---
+++ project.clj
@@ -0,0 +1,11 @@
+(defproject nephrite "0.1.0-SNAPSHOT"
+  :description "A tools for developing jade applications."
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [clj-jade "0.1.4"]
+                 [ring "1.2.0"]
+                 [compojure "1.1.5"]
+                 [com.stuartsierra/flow "0.1.0"]
+                 [cheshire "5.2.0"]]
+  :main nephrite.main)
